ab3f3cff3d58ebdd6107193649b8bf27ce52389e
Andrea Valenza <avalenza89@gmail.com>
2017-02-01 13:33:46 +0100

    Update orchestrator.sh

--- orchestrator.sh
+++ orchestrator.sh
@@ -2,5 +2,6 @@
 
 docker run -v $(pwd):/files hlpsl2 $1
 file=${1/\.hlpsl/\.if}
-docker run -v $(pwd):/files satmc /files/$file 
+docker run -v $(pwd):/files satmc /files/$file
+docker run -v $(pwd):/files ofmc /files/$file
 docker run -v $(pwd):/files -it ta4sp /files/$file

d1cbcefd695760797c15cc6e3e369ea3dc044d6f
avalz <avalenza89@gmail.com>
2018-08-23 11:00:19 +0200

    Add cl-atse

--- orchestrator.sh
+++ orchestrator.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-docker run -v $(pwd):/files hlpsl2 $1
+docker run -v $(pwd):/files hlpsl2if $1
 file=${1/\.hlpsl/\.if}
-docker run -v $(pwd):/files satmc /files/$file
+docker run -v $(pwd):/files avispaproject/satmc:1.0 /files/$file
+docker run -v $(pwd):/files turuani/cl-atse:latest /files/$file
 docker run -v $(pwd):/files ofmc /files/$file
 docker run -v $(pwd):/files -it ta4sp /files/$file
